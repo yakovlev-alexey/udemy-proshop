d911258b2d85e81752ff66e27b6179eaffaeb712
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-10-09 21:16:27 +0300

    Implement Header, Footer and basic App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import './bootstrap.min.css'
+
 const App = () => {
-  return <React.Fragment></React.Fragment>
+  return (
+    <React.Fragment>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </React.Fragment>
+  )
 }
 
 export default App
