27ed4f1004f98575342c196d698d775a4ca8af05
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-10-09 21:28:14 +0300

    Implement basic product cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen'
+
 import './bootstrap.min.css'
 
 const App = () => {
@@ -13,7 +15,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Row, Col } from 'react-bootstrap'
+
+import Product from '../components/Product'
+
+import products from '../products'
+
+const HomeScreen = () => {
+  return (
+    <React.Fragment>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </React.Fragment>
+  )
+}
+
+export default HomeScreen
