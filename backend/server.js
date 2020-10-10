2a13a8531530276a5001ff2229cc328d401076b3
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-10-10 13:47:24 +0300

    Add connection to MongoDB Atlas and colorize server console

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+
+import connectDB from './config/db.js'
 
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,5 +23,5 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold)
 })
