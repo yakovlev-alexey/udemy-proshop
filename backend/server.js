eb1d2e1cc221e8d9cae67a2e9c4973e7c9e69584
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-10-10 14:49:30 +0300

    Implement basic backend error handling

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,24 @@ import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
 
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 dotenv.config()
 
+// initialize a connection to MongoDB Atlas
 connectDB()
 
 const app = express()
 const port = process.env.PORT || 5000
 
+// setup routers
 app.use('/api/products', productRoutes)
 
+// setup middleware
+app.use(notFound)
+app.use(errorHandler)
+
+// initialize server
 app.listen(port, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold)
 })
