6b5c2d672c16b0706ce99af1ffb6556e7c3bd399
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2020-10-29 16:32:15 +0300

    Implement create and update product backend endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,6 +10,55 @@ export const getProducts = asyncHandler(async (req, res) => {
   res.json(products)
 })
 
+// @desc    Create a new product
+// @route   POST /api/products
+// @access  Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description'
+  })
+
+  const createdProduct = await product.save()
+
+  if (createdProduct) {
+    res.status(201).json(createdProduct)
+  }
+})
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, brand, category, countInStock, description } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name || product.name
+    product.price = price || product.price
+    product.image = image || product.image
+    product.brand = brand || product.brand
+    product.category = category || product.category
+    product.countInStock = countInStock || product.countInStock
+    product.description = description || product.description
+
+    const updatedProduct = await product.save()
+
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
